deefde6946e55a172c9a9bf763142efe01984cc2
mark burdett <mfb@eff.org>
2016-08-05 10:04:54 -0700

    fix formatting

--- routes/notifications/get_notification.js
+++ routes/notifications/get_notification.js
@@ -7,24 +7,22 @@ var hapi = require('hapi');
 var logger = require('log4js').getLogger('server');
 var models = require('../../db/models');
 
-
 var getNotification = function(request, reply) {
   var notificationId = request.params.notificationId;
 
   models.Notifications
     .find({where: {notificationId: notificationId}})
-    .then(function(notification) {
+    .then(function (notification) {
       if (notification !== null) {
         reply(notification.externalize());
       }
-       else {
+      else {
         reply(boom.badImplementation('notification not found', notificationId));
       }
-    }, function(err) {
+    }, function (err) {
       logger.error('unable to fetch notification for id %s, err:\n %s', notificationId, err);
-      reply(boom.badImplementation('unable to fetch notification for id', [notificationId, err]));
+      reply(boom.badImplementation('unable to fetch notification', [notificationId, err]));
     });
 };
-
 
 module.exports = getNotification;
