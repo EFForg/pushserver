39e48a814bda2564f6e7955d3c9e139ae05fe713
Peter Woo <peterw@eff.org>
2018-01-10 11:41:16 -0800

    Fix send_test_gcm_notification.js

--- bin/send_test_gcm_notification.js
+++ bin/send_test_gcm_notification.js
@@ -3,31 +3,44 @@
  */
 
 var config = require('config');
+var firebase = require('firebase-admin');
 
-var GCMDispatcher = require('../plugins/push/apns_dispatcher');
+firebase.initializeApp({
+  credential: firebase.credential.cert({
+    projectId: config.CREDENTIALS.FCM.PROJECT_ID,
+    clientEmail: config.CREDENTIALS.FCM.CLIENT_EMAIL,
+    privateKey: config.CREDENTIALS.FCM.PRIVATE_KEY.replace(/\\n/g, "\n")
+  }),
+  databaseURL: config.CREDENTIALS.FCM.DATABASE_URL
+});
 
-var credentials = config.get('CREDENTIALS');
-var gcmCreds = {
-  projectId: credentials.get('GCM').get('PROJECT_ID'),
-  apiKey: credentials.get('GCM').get('API_KEY'),
-  mode: config.get('MODE')
-}
+var registrationIds = [];
+var topic = null;
 
-var gcmPushDispatcher = new GCMDispatcher(gcmCreds);
-
-var registrationIds = [
-  'ENTER_A_REGISTRATION_ID'
-];
 var gcmMessage = {
-  dry_run: false,
   data: {
     message: 'this is a test message',
     title: 'this is a test message title',
     url: 'https://www.google.com'
   }
 };
-var done = function() {
-  console.log('message sent!');
-};
 
-gcmPushDispatcher.dispatch(registrationIds, gcmMessage, done);
+if (topic) {
+  firebase.messaging().sendToTopic(topic, gcmMessage)
+    .then(function(response) {
+      console.log("Successfully sent message:", response);
+    })
+    .catch(function(error) {
+      console.log("Error sending message:", error);
+    });
+}
+
+if (registrationIds.length) {
+  firebase.sendToDevice(registrationIds, gcmMessage)
+    .then(function(response) {
+      console.log("Successfully sent message:", response);
+    })
+    .catch(function(error) {
+      console.log("Error sending message:", error);
+    });
+}
